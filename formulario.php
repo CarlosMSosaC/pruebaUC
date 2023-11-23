1f27fa5ee151badb32b917066e03ace26ed90df9
CarlosMSosaC <151872209+CarlosMSosaC@users.noreply.github.com>
2023-11-23 13:43:12 -0500

    Version1

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<form action="procesar.php" method="post">
+        <label>Nombre:</label>
+        <input type="text" name="nombre">
+        <br>
+        <label>Edad</label>
+        <input type="text" name="edad">
+        <br>
+        <label>Email</label>
+        <input type="email" name="email">
+        <input type="submit" name="Enviar">
+        <br>
+       
+    </form>
---
+++ procesar.php
@@ -0,0 +1,7 @@
+<?php
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+$email = $_POST['email'];
+
+echo "su nombre es:" .$nombre."  su edad es:" .$edad.  "y su email es:"  .$email;
+?>
